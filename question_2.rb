3b063bad9e21262fd2d49ef164c082d5374da551
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-10-28 15:19:02 -0300

    rule align padding

--- question_2.rb
+++ question_2.rb
@@ -20,6 +20,17 @@ def rule_length(str, length)
   str.slice(0..length - 1)
 end
 
+def rule_align_padding(str, align, padding, length)
+  padding_opt = {
+    spaces: ' ',
+    zeroes: '0'
+  }
+  length_padding = str.length + length
+  return str.ljust(length_padding, padding_opt[padding.to_sym]) if align == 'left'
+
+  return str.rjust(length_padding, padding_opt[padding.to_sym]) if align == 'right'
+end
+
 def validates(item)
   concat_str = ''
   item.each do |k, v|
@@ -28,8 +39,14 @@ def validates(item)
 
     length = key_validations['length']
     result = rule_length(v, length)
-    puts result
+    if result.length < length
+      padding = key_validations['padding']
+      align = key_validations['align']
+      result = rule_align_padding(result, align, padding, length)
+    end
+    concat_str += result
   end
+  concat_str
 end
 
 input.each do |item|
